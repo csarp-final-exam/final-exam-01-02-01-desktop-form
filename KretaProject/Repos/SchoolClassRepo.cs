0db7a73cbf5b27a4c9805f8c1f0225f851d89806
Csaba Gyuris <gyuris.csaba@vasvari.org>
2025-03-05 15:07:55 +0100

    SchoolClassRepo

---
+++ KretaProject/Repos/SchoolClassRepo.cs
@@ -0,0 +1,32 @@
+﻿using KretaProject.Models;
+
+namespace KretaProject.Repos
+{
+    public class SchoolClassRepo
+    {
+        private List<SchoolClass> schoolClasses = new List<SchoolClass>();
+
+        public void AddClass(SchoolClass schoolClass)
+        {
+            if (schoolClass == null)
+                throw new ArgumentNullException(nameof(schoolClass));
+
+            schoolClasses.Add(schoolClass);
+        }
+
+        public bool UpdateClassName(int id, string newClassId)
+        {
+            var schoolClass = schoolClasses.FirstOrDefault(sc => sc.Id == id);
+            if (schoolClass == null || string.IsNullOrWhiteSpace(newClassId))
+                return false;
+
+            schoolClass.ClassId = newClassId;
+            return true;
+        }
+
+        public List<SchoolClass> GetAllClasses()
+        {
+            return schoolClasses;
+        }
+    }
+}
